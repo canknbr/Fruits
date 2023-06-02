75b0692884e399694422fe3698da5bbbfc63b9ec
canknbr <cankanbur1@gmail.com>
2023-06-02 09:41:16 +0300

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Can Kanbur on 2.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
